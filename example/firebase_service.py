5a292d18d60e303fa5cc0d0f0d678b912d040774
Prayag Bhosale <prayagbhitsa@gmail.com>
2025-04-07 09:47:06 +0530

    refactor: env vars

--- example/firebase_service.py
+++ example/firebase_service.py
@@ -1,14 +1,30 @@
 import firebase_admin
 from firebase_admin import credentials, db
 import os
+from dotenv import load_dotenv
 
-# Build path to JSON key
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-cred_path = os.path.join(BASE_DIR, "serviceAccountKey.json")
+# Load environment variables from .env file
+load_dotenv()
+
+# Build credentials from environment variables
+cred = credentials.Certificate(
+    {
+        "type": os.getenv("TYPE"),
+        "project_id": os.getenv("PROJECT_ID"),
+        "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+        "private_key": os.getenv("PRIVATE_KEY").replace("\\n", "\n"),
+        "client_email": os.getenv("CLIENT_EMAIL"),
+        "client_id": os.getenv("CLIENT_ID"),
+        "auth_uri": os.getenv("AUTH_URI"),
+        "token_uri": os.getenv("TOKEN_URI"),
+        "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+        "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
+        "universe_domain": os.getenv("UNIVERSE_DOMAIN"),
+    }
+)
 
 # Initialize Firebase app only once
 if not firebase_admin._apps:
-    cred = credentials.Certificate(cred_path)
     firebase_admin.initialize_app(
         cred, {"databaseURL": "https://spendly-e467c-default-rtdb.firebaseio.com"}
     )
